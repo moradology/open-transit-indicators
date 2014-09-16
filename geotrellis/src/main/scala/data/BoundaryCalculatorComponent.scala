41d7d94dbe2d7b30029bb30f17adb78b3fec50d4
Derek Dohler <ddohler@azavea.com>
2014-09-16 11:22:30 -0400

    Add 500m coverage ratio indicator

---
+++ geotrellis/src/main/scala/data/BoundaryCalculatorComponent.scala
@@ -0,0 +1,50 @@
+package opentransitgt.data
+
+import com.azavea.gtfs._
+import scala.slick.driver.PostgresDriver
+import geotrellis.vector._
+import geotrellis.slick._
+import opentransitgt.IndicatorCalculator
+
+/**
+ * A trait providing Boundaries to an IndicatorCalculator
+ */
+trait BoundaryCalculatorComponent {this: IndicatorCalculator =>
+
+  // Wrap Slick persistence items to prevent potential naming conflicts.
+  object bufSlick {
+    val profile = PostgresDriver
+    val gis = new PostGisProjectionSupport(profile)
+  }
+  import bufSlick.profile.simple._
+  import bufSlick.gis._
+
+
+  /**
+   * A boundary, of either a city or a region.
+   */
+  case class Boundary(
+    id: Int,
+    geom: Projected[MultiPolygon] // Location-dependent SRID (UTM zone)
+  )
+  
+  /**
+   * Table class supporting Slick persistence
+   */
+  class Boundaries(tag: Tag) extends Table[Boundary](tag, "utm_datasources_boundary") {
+    def id = column[Int]("boundary_id")
+    def geom = column[Projected[MultiPolygon]]("geom")
+
+    def * = (id, geom) <> (Boundary.tupled, Boundary.unapply)
+  }
+
+  def boundaryTable = TableQuery[Boundaries]
+  /**
+   * Returns a Boundary (geometry denoting a city or region boundary)
+   */
+  def boundaryWithId(boundaryId: Int): Option[Boundary] = {
+    db withSession { implicit session: Session =>
+      boundaryTable.filter(_.id === boundaryId).firstOption
+    }
+  }
+}
