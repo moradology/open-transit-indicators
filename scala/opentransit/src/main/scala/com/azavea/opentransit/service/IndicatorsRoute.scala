5d8a9f9b9feb252efffb7213f3f1920f9d196886
Nathan Zimmerman <npzimmerman@gmail.com>
2014-10-16 13:53:42 -0400

    init

--- scala/opentransit/src/main/scala/com/azavea/opentransit/service/IndicatorsRoute.scala
+++ scala/opentransit/src/main/scala/com/azavea/opentransit/service/IndicatorsRoute.scala
@@ -1,5 +1,6 @@
 package com.azavea.opentransit.service
 
+import scala.util.{Try, Success, Failure}
 import com.azavea.opentransit._
 import com.azavea.opentransit.CalculationStatus
 import com.azavea.opentransit.CalculationStatus._
@@ -32,13 +33,14 @@ import SprayJsonSupport._
 import DefaultJsonProtocol._
 
 import com.typesafe.config.{ConfigFactory, Config}
-
+import scala.concurrent._
 case class IndicatorJob(
   version: String = "",
   status: Map[String, CalculationStatus]
 )
 
 trait IndicatorsRoute extends Route { self: DatabaseInstance =>
+  implicit val dispatcher: ExecutionContext
   val config = ConfigFactory.load
   val dbGeomNameUtm = config.getString("database.geom-name-utm")
 
@@ -52,54 +54,67 @@ trait IndicatorsRoute extends Route { self: DatabaseInstance =>
       post {
         entity(as[IndicatorCalculationRequest]) { request =>
           complete {
-            var err: JsObject = null
             TaskQueue.execute {
-              try {
-                // Load Gtfs records from the database. Load it with UTM projection (column 'geom' in the database)
-                val gtfsRecords =
-                  db withSession { implicit session =>
-                    GtfsRecords.fromDatabase(dbGeomNameUtm)
-                  }
 
-                // Perform all indicator calculations, store results and statuses
-                CalculateIndicators(request, gtfsRecords, db, new CalculationStatusManager {
-                  def indicatorFinished(containerGenerators: Seq[ContainerGenerator]) = {
-                    val indicatorResultContainers = containerGenerators.map(_.toContainer(request.version))
-                    DjangoClient.postIndicators(request.token, indicatorResultContainers)
-                  }
+              def calculationTask =
+                Try {
+                  // Load Gtfs records from the database. Load it with UTM projection (column 'geom' in the database)
+                  val gtfsRecords =
+                    db withSession { implicit session =>
+                      GtfsRecords.fromDatabase(dbGeomNameUtm)
+                    }
 
-                  def statusChanged(status: Map[String, CalculationStatus]) = {
-                    DjangoClient.updateIndicatorJob(request.token, IndicatorJob(request.version, status))
-                  }
-                })
-              } catch {
-              case e: Exception =>
+                  // Perform all indicator calculations, store results and statuses
+                  CalculateIndicators(request, gtfsRecords, db, new CalculationStatusManager {
+                    def indicatorFinished(containerGenerators: Seq[ContainerGenerator]) = {
+                      val indicatorResultContainers = containerGenerators.map(_.toContainer(request.version))
+                      DjangoClient.postIndicators(request.token, indicatorResultContainers)
+                    }
+
+                    def statusChanged(status: Map[String, CalculationStatus]) = {
+                      DjangoClient.updateIndicatorJob(request.token, IndicatorJob(request.version, status))
+                    }
+                  })
+
+                  Created -> JsObject(
+                      "success" -> JsBoolean(true),
+                      "message" -> JsString(s"Calculations started (version ${request.version})")
+                  )
+                }
+
+              def failedCalculation(e: Exception) {
                 println("Error calculating indicators!")
                 println(e.getMessage)
                 println(e.getStackTrace.mkString("\n"))
-                err = JsObject(
-                  "success" -> JsBoolean(false),
-                  "message" -> JsString("No GTFS data")
-                )
-
                 // update status to indicate indicator calculation failure
-                try {
-                  DjangoClient.updateIndicatorJob(request.token, IndicatorJob(request.version, Map("alltime" -> CalculationStatus.Failed)))
-                } catch {
-                  case ex: Exception =>
-                    println("Failed to set failure status for indicator calculation job!")
-                }
-              }
-            }
+                def djangoUpdateStatusTask =
+                  Try {
+                    DjangoClient.updateIndicatorJob(
+                        request.token,
+                        IndicatorJob(
+                            request.version,
+                            Map("alltime" -> CalculationStatus.Failed)
+                        )
+                    )
+                  }
 
-            if (err == null) {
-              // return a 201 created
-              Created -> JsObject(
-                "success" -> JsBoolean(true),
-                "message" -> JsString(s"Calculations started (version ${request.version})")
-              )
-            } else {
-              err
+                djangoUpdateStatusTask match {
+                  case Success(result) => result
+                  case Failure(e: Exception) => {
+                    println("Failed to set failure status for indicator calculation job!")
+                  }
+                }
+                JsObject(
+                    "success" -> JsBoolean(false),
+                    "message" -> JsString("No GTFS data")
+                )
+              }
+
+
+              calculationTask match {
+                case Success(json) => json
+                case Failure(e: Exception) => failedCalculation(e)
+              }
             }
           }
         }
